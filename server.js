505d2f547c364fbdb8ab22ce370099d8b32e79c3
Sumanta Kumar Mohanta <mohantasumanta601@gmail.com>
2026-04-13 14:33:20 +0530

    remove qn comments

--- server.js
+++ server.js
@@ -18,26 +18,6 @@ app.get("/people", (req, res) => {
 
 app.get("/api", (req, res) => {
 	let filteredData = startups;
-	// TODO: based on the query serve the data to user
-	/*
-	Challenge:
-	1. When a user hits the /api endpoint with query params, filter the data so 
-	we only serve objects that meet their requirements. 
-		 
-	The user can filter by the following properties:
-	  industry, country, continent, is_seeking_funding, has_mvp
-	
-	Test Cases
-	
-	/api?industry=renewable%20energy&country=germany&has_mvp=true
-	  Should get the "GreenGrid Energy" object.
-	
-	/api?industry=renewable%20energy&country=germany&has_mvp=false
-	  Should not get any object
-	
-	/api?continent=asia&is_seeking_funding=true&has_mvp=true
-	  should get for objects with IDs 3, 22, 26, 29
-	*/
 	console.log(req.query);
 	let { industry, country, continent, is_seeking_funding, has_mvp
 	} = req.query;
