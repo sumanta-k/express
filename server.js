59710905dc362fdce252fb173c4522bb34567df4
Sumanta Kumar Mohanta <mohantasumanta601@gmail.com>
2026-04-13 15:43:25 +0530

    INFO: added how to stop server inside commandline

--- server.js
+++ server.js
@@ -64,4 +64,4 @@ app.get('/', (req, res) => {
 	res.json(celebrity);
 });
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}. Use Ctrl + C to stop it.`));
