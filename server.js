9a54c56639c1a8d4d4c2ffbe7f74032b7f609c44
Sumanta Kumar Mohanta <mohantasumanta601@gmail.com>
2026-04-13 10:34:05 +0530

    add: data to serve to user
    when user hits '/'

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ import express from "express";
 
 const PORT = 8000;
 
+const celebrity = {
+	type: "action hero",
+	name: "JSON Statham"
+};
+
 const app = express();
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
