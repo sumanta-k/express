0b2359d4c5bf9156393d43214b5e557335517533
Sumanta Kumar Mohanta <mohantasumanta601@gmail.com>
2026-04-13 11:44:55 +0530

    qn: serve based on the query

--- server.js
+++ server.js
@@ -17,6 +17,26 @@ app.get("/people", (req, res) => {
 });
 
 app.get("/api", (req, res) => {
+	// TODO: based on the query serve the data to user
+	/*
+	Challenge:
+	1. When a user hits the /api endpoint with query params, filter the data so 
+	we only serve objects that meet their requirements. 
+		 
+	The user can filter by the following properties:
+	  industry, country, continent, is_seeking_funding, has_mvp
+	
+	Test Cases
+	
+	/api?industry=renewable%20energy&country=germany&has_mvp=true
+	  Should get the "GreenGrid Energy" object.
+	
+	/api?industry=renewable%20energy&country=germany&has_mvp=false
+	  Should not get any object
+	
+	/api?continent=asia&is_seeking_funding=true&has_mvp=true
+	  should get for objects with IDs 3, 22, 26, 29
+	*/
 	res.json(startups);
 });
 
